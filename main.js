7d1a21d0b9ee4ae941931b921ed49d014d30607e
julien <julien.genestoux@gmail.com>
2013-06-22 00:34:29 +0200

    actually removed express

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var xmpp = require('node-xmpp');
 
-var r = new xmpp.Router();
+var r = new xmpp.Router(process.env.PORT);
 r.register('notifix.superfeedr.com', function(stanza) {
     console.log("<< "+stanza.toString());
     if (stanza.attrs.type !== 'error') {
@@ -10,3 +10,4 @@ r.register('notifix.superfeedr.com', function(stanza) {
   r.send(stanza);
     }
 });
+// So, where do we point this to?
