178ec94943b28e9088fdb0bb077bdb562d55a110
julien <julien.genestoux@gmail.com>
2013-06-22 09:29:42 +0200

    cleanup

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 var xmpp = require('node-xmpp');
 
+var jid = 'notifix.superfeedr.com';
+
 var r = new xmpp.Router(process.env.PORT);
+r.loadCredentials(jid, jid + '.key',  jid + '.crt');
+
 r.register('notifix.superfeedr.com', function(stanza) {
     console.log("<< "+stanza.toString());
     if (stanza.attrs.type !== 'error') {
@@ -11,3 +15,8 @@ r.register('notifix.superfeedr.com', function(stanza) {
     }
 });
 // So, where do we point this to?
+
+
+// SRV _xmpp-client._tcp.notifix.superfeedr.com  60    10 5222 69.164.222.83
+// SRV _xmpp-server._tcp.notifix.superfeedr.com  60    10 5269 69.164.222.83
+// SRV _jabber._tcp.notifix.superfeedr.com 600   10 5269 69.164.222.83
