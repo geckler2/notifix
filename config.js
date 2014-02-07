8d1cef431f91c7b9127e2771d13cada946f3d57c
geckler2 <bachelortrades@gmail.com>
2014-02-07 04:01:44 -0500

    Create config.js

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  xmpp: {
+    jid: 'notifix.xmpp.server.com',
+    host: 'xmpp.server.com',
+    port: '8876',
+    password: ''
+  },
+  irc: {
+    channels: ["#notifix"],
+    server: "irc.freenode.net",
+    botName: "notifix"
+  }
+}
